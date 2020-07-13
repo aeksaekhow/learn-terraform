29da1e32410cb0d6b3feefc9c1c5fcad4e3706d8
Aek Sae-khow <saekhow.aek@gmail.com>
2020-07-13 22:44:29 +0700

    Add rsa key-pair and create ngix

--- learn-terraform-aws-instance/main.tf
+++ learn-terraform-aws-instance/main.tf
@@ -3,11 +3,28 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+resource "aws_key_pair" "example" {
+  key_name   = "examplekey"
+  public_key = file("~/.ssh/terraform_rsa.pub")
+}
+
 resource "aws_instance" "example" {
-  ami           = "ami-b374d5a5"
+  key_name      = aws_key_pair.example.key_name
+  ami           = "ami-04590e7389a6e577c"
   instance_type = "t2.micro"
 
-  provisioner "local-exec" {
-      command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("~/.ssh/terraform_rsa")
+    host        = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo amazon-linux-extras enable mginx1.12",
+      "sudo yum -y install nginx",
+      "sudo systemctl start nginx"
+    ]
   }
 }
