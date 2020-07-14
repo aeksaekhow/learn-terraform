3c100e6403b38207e7489e70b330e09630e1bcd9
Aek Sae-khow <saekhow.aek@gmail.com>
2020-07-14 15:56:38 +0700

    Use Terraform cloud

--- learn-terraform-aws-instance/main.tf
+++ learn-terraform-aws-instance/main.tf
@@ -1,30 +1,9 @@
-provider "aws" {
-  profile = "default"
-  region  = "us-east-1"
-}
+terraform {
+  backend "remote" {
+    organization = "MemoFive"
 
-resource "aws_key_pair" "example" {
-  key_name   = "examplekey"
-  public_key = file("~/.ssh/terraform_rsa.pub")
-}
-
-resource "aws_instance" "example" {
-  key_name      = aws_key_pair.example.key_name
-  ami           = "ami-04590e7389a6e577c"
-  instance_type = "t2.micro"
-
-  connection {
-    type        = "ssh"
-    user        = "ec2-user"
-    private_key = file("~/.ssh/terraform_rsa")
-    host        = self.public_ip
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo amazon-linux-extras enable mginx1.12",
-      "sudo yum -y install nginx",
-      "sudo systemctl start nginx"
-    ]
+    workspaces {
+      name = "Example-Workspace"
+    }
   }
 }
